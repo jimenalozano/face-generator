3792904233131bae853071954267a1d7fe4a5dd5
jimenalozano <jilozano@itba.edu.ar>
2021-05-09 16:51:41 -0300

    stylegan2encoder fixes

--- stylegan2/stylegan2encoder/encoder/generator_model.py
+++ stylegan2/stylegan2encoder/encoder/generator_model.py
@@ -29,7 +29,11 @@ class Generator:
         self.sess = tf.get_default_session()
         self.graph = tf.get_default_graph()
 
-        self.dlatent_variable = [v for v in tf.global_variables() if 'learnable_dlatents' in v.name]
+        for v in tf.global_variables():
+            if 'learnable_dlatents' in v.name:
+                self.dlatent_variable = v
+                break
+
         self.set_dlatents(self.initial_dlatents)
 
         self.generator_output = self.graph.get_tensor_by_name('G_synthesis_1/_Run/concat:0')
