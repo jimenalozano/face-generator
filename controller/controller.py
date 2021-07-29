5248c94dc1d6ea0c5ed64f884d9299e24f3d93b3
jimenalozano <jilozano@itba.edu.ar>
2021-07-29 19:14:51 -0300

    modularization in controller

--- controller/controller.py
+++ controller/controller.py
@@ -1,35 +1,43 @@
 import sys
 
+import numpy as np
+
 from persistance.generator_seeds import GeneratorSeedsDb
 
 sys.path.insert(0, "../generator")
 
 from generator import Generator
 
+generator = Generator(num_gpus=1,
+                      results_dir_root='results',
+                      network_pkl='gdrive:networks/stylegan2-ffhq-config-f.pkl')
 
-def main(db: GeneratorSeedsDb):
-    generator = Generator(num_gpus=1,
-                          results_dir_root='results',
-                          network_pkl='gdrive:networks/stylegan2-ffhq-config-f.pkl')
+db = GeneratorSeedsDb()
+db.open_sql_connection()
+db.create_sql_table_seeds()
 
-    #   Generating random images
-    print("Generating random images")
-    seeds = generator.generate_random_images(qty=10, seed_from=8000, dlatents=False)
+
+def generate_random_images(qty: int):
+    print("Generating random images.....")
+    seed_from = np.random.randint(30000)
+    seeds = generator.generate_random_images(qty=qty, seed_from=seed_from, dlatents=False)
     db.insert_seeds(seeds)
+    return db.fetch_all()
 
-    #   Examining the latent space
-    print("Examining the latent space")
-    generator.generate_transition(seed_from=8192, seed_to=8201, steps=100, path='results/transition')
 
-    #   Adding noise
-    print("Adding noise")
-    generator.generate_noise(seed=500, path='results/noise')
+def generate_transition(id_img1: int, id_img2: int = None):
+    if id_img2 is None:
+        all_images = db.fetch_all()
+        id_img2 = all_images[np.random.randint(len(all_images))][0]
+
+    print("Generating transition from image " + str(id_img1) + " to image " + str(id_img2))
+
+    seed_1 = db.fetch_id(id_img1)
+    seed_2 = db.fetch_id(id_img2)
+
+    generator.generate_transition(seed_from=seed_1, seed_to=seed_2, steps=100, path='results/transition')
 
 
 if __name__ == "__main__":
-
-    db = GeneratorSeedsDb()
-    db.open_sql_connection()
-    db.create_sql_table_seeds()
-
-    main(db)
+    generate_random_images(10)
+    generate_transition(1, 2)
