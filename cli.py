0bc38ded31326221569c0f8a3a81329cbb0825cf
jimenalozano <jilozano@itba.edu.ar>
2021-08-30 04:36:40 +0000

    minor fixes

--- cli.py
+++ cli.py
@@ -2,10 +2,10 @@ import os
 
 # Checking if stylegan2 is installed
 os.system('echo Checking if stylegan2 is installed...')
-os.system('cd stylegan2')
+os.system('cd src/stylegan2encoder')
 os.system('ls')
 
 # Checking hardware requirements
 os.system('echo ')
 os.system('echo Checking hardware requirements...')
-os.system('nvcc stylegan2/test_nvcc.cu -o test_nvcc -run')
+os.system('nvcc src/stylegan2encoder/test_nvcc.cu -o test_nvcc -run')
--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-gunicorn --bind 0.0.0.0:5000 --workers 1 --timeout 4800 face_generator:app
+gunicorn --bind 0.0.0.0:5000 --workers 2 --timeout 4800 face_generator:app
